a03a3eb67d629ba8a3cf404f5de53b501185ad73
corrinep <corrinepeterkin.optimum.net>
2018-09-18 10:12:15 -0400

    update

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var targetNumber = function(){
 }
 console.log("targetNumber");
 
-$("#number-to-guess").text(targetNumber);
+$("number-to-guess").text(targetNumber);
 
 var counter = 0;
 
@@ -19,14 +19,3 @@ var num3 = math.floor(math.random() * 11 + 1);
 var num4 = math.floor(math.random() * 11 + 1);
   
 });
-
-var imageCrystalOne = $("<img>");
-var imageCrystalTwo = $("<img>");
-var imageCrystalThree = $("<img>");
-var imageCrystalFour = $("<img>");
-
-imageCrystalOne.addClass("crystal-image");
-imageCrystalTwo.addClass("crystal-image");
-imageCrystalThree.addClass("crystal-image");
-imageCrystalFour.addClass("crystal-image");
-
